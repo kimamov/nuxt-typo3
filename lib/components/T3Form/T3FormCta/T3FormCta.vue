2df268cb0566741419b84c44a5a20ec03f56bef7
Adam <a.marcinkowski@macopedia.com>
2021-11-04 13:40:23 +0100

    feat(components): add new form framework (#192)

---
+++ lib/components/T3Form/T3FormCta/T3FormCta.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="t3-form-cta">
+    <button type="reset">
+      {{ i18n.resetButton }}
+    </button>
+    <button type="submit" :disabled="state.sending">
+      <template v-if="state.sending">
+        {{ i18n.sendingLabel }}
+      </template>
+      <template v-else emplate>
+        {{ i18n.submitButton }}
+      </template>
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'T3FormCta',
+  props: {
+    state: {
+      type: Object,
+      required: true
+    },
+    i18n: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
